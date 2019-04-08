9fb97a4e603dc9f4bf4fdc3cda23e1033b60da6c
Ricafelle Dorothy Z. Fortes <rdfortes.upc@gmail.com>
2019-04-08 16:00:11 +0800

    First commit

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_user_can_view_a_login_form() {
+        $response = $this->get('/login');
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
+
+     public function test_user_can_login()
+    {
+        $email     = 'test@laravel.com';
+        $password  = 'secret';
+
+        $user = [
+            'email' => $email,
+            'password' => $password
+        ];
+
+        $response = $this->post('/login', $user);
+
+        $response->assertRedirect('/');
+    }
+}
